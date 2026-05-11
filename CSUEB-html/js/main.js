25a503a6d606b7294b6ff9282730ccfd9fab9a2f
vpatoleexemplifi <vpatole@exemplifi.io>
2026-05-11 15:39:19 +0530

    Events style updated

--- CSUEB-html/js/main.js
+++ CSUEB-html/js/main.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", () => {
   initMainImgSlider();
   initGalleryLightbox();
   initAccessibilityFeatures();
-
+  initFlatpickrDateInputs();
 
   const submenuItems = document.querySelectorAll('.leftnav .sidenav .drop:not(.active) .sidenav-sub');
   submenuItems.forEach(submenuItem => {
@@ -986,6 +986,7 @@ function initGalleryLightbox() {
     youtubePlayers = [];
   });
 }
+
 
 
 // Accessibility Features
